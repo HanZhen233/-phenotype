4cf98c123c4fc2d2b354c314600a8e2934304403
HanZhen223 <1005256626@qq.com>
2018-08-05 20:08:31 +0800

    解决返回按钮返回上一缓存链接的问题

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,9 +1,28 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+  {
+    "id": "cordova-plugin-device.device",
+    "file": "plugins/cordova-plugin-device/www/device.js",
+    "pluginId": "cordova-plugin-device",
+    "clobbers": [
+      "device"
+    ]
+  },
+  {
+    "id": "cordova-plugin-backbutton.Backbutton",
+    "file": "plugins/cordova-plugin-backbutton/www/Backbutton.js",
+    "pluginId": "cordova-plugin-backbutton",
+    "clobbers": [
+      "navigator.Backbutton"
+    ]
+  }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-whitelist": "1.3.3"
+  "cordova-plugin-whitelist": "1.3.3",
+  "cordova-plugin-device": "2.0.2",
+  "cordova-plugin-backbutton": "0.3.0"
 };
 // BOTTOM OF METADATA
 });

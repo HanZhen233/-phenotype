50af9b5db4b5ad181c0ebfd39d82fbdf4a8373b0
HanZhen223 <1005256626@qq.com>
2018-08-07 21:02:58 +0800

    更改主界面跳转方式，并完成数据传输的基本界面响应

---
+++ platforms/android/assets/www/js/skip.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $("#page_index").click(function () {
+       location.href="index.html";
+    });
+    $("#page_dataTransmission").click(function () {
+        location.href="dataTransmission.html";
+    });
+    $("#page_setting").click(function () {
+        location. href="setting.html" ;
+    });
+})
---
+++ www/js/start-server.js
@@ -0,0 +1,8 @@
+$(document).ready(
+    function () {
+        $("#connect").click(function () {
+            document.getElementById("show_tips").innerText="点击确认后在电脑的浏览器中访问以下地址";
+            document.getElementById("url").innerText="192.168.99.158:15000";
+        })
+    }
+)

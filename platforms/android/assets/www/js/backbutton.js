bb87b90496224e690f93ac78907de727ab5f7652
HanZhen223 <1005256626@qq.com>
2018-09-27 11:29:05 +0800

    -m忽略部分文件夹

--- platforms/android/assets/www/js/backbutton.js
+++
@@ -1,9 +0,0 @@
-// 等待加载PhoneGap
-document.addEventListener("deviceready",onDeviceReady,false);
-function onDeviceReady() {
-    document.addEventListener("backbutton",onBackKeyDown,false);
-};
-function onBackKeyDown() {
-
-    navigator.app.exitApp();
-}
